aa5cefbc4915f1ab15eb98a3ff8731a694a40649
gauravxdhingra <grvdhingra1999@gmail.com>
2020-06-01 18:45:16 +0530

    Joystick And Controls

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    public Joystick joystick;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _xMovementInput = joystick.Horizontal;
+        float _zMovementInput = joystick.Vertical;
+
+        Vector3 _movementHorizontal = transform.right * _xMovementInput;
+        Vector3 _movementVertical = transform.forward * _zMovementInput;
+
+        Vector3 _movementVelocityVector = (_movementHorizontal+_movementVertical ).normalized * speed;
+
+        
+    }
+}
