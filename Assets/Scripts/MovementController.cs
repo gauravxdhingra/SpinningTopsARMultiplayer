03f82df047ccfcdb505c0571d2dfdb8bcf1fd647
gauravxdhingra <grvdhingra1999@gmail.com>
2020-06-02 08:31:26 +0530

    Movement And Joystick Complete

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -6,25 +6,55 @@ public class MovementController : MonoBehaviour
 {
 
     public Joystick joystick;
-    public float speed;
+    public float speed=4;
+    private Vector3 velocityVector=Vector3.zero;//Initial Velocity
+    private Rigidbody rb;
+    public float maximumVelocityChange=4;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Joystick input
         float _xMovementInput = joystick.Horizontal;
         float _zMovementInput = joystick.Vertical;
 
+        // Calc velocity vector
         Vector3 _movementHorizontal = transform.right * _xMovementInput;
         Vector3 _movementVertical = transform.forward * _zMovementInput;
 
+        // Final movement velocity vector
         Vector3 _movementVelocityVector = (_movementHorizontal+_movementVertical ).normalized * speed;
 
+        // Apply movement
+        Move(_movementVelocityVector);
+    }
+
+    void Move(Vector3 movementVelocityVector){
+         velocityVector = movementVelocityVector;
+    }
+
+    private void FixedUpdate() {
+        if(velocityVector!=Vector3.zero){
+
+            // get rigidbody's current velocity
+            Vector3 velocity=rb.velocity;
+            Vector3 velocityChange = (velocityVector-velocity);
         
+
+        velocityChange.x =Mathf.Clamp(velocityChange.x,-maximumVelocityChange, maximumVelocityChange );
+        velocityChange.z =Mathf.Clamp(velocityChange.z,-maximumVelocityChange, maximumVelocityChange );
+
+        velocityChange.y=0f;
+
+        rb.AddForce(velocityChange,ForceMode.Acceleration);
+
+        }
     }
 }
