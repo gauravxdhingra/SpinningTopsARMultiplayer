dc6188a3bd415ebd02a3a19f83b4106478910bd4
gauravxdhingra <grvdhingra1999@gmail.com>
2020-06-02 23:09:49 +0530

    Photon Servers

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    [Header("Login UI")]
+
+    public InputField playerNameInputField;
+
+    #region UNITY Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion 
+
+    #region UI Callback Methods
+    public void onEnterGameButtonClicked()
+    {
+        string playerName = playerNameInputField.text;
+
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        else
+        {
+            Debug.Log("Player Name Is Invalid");
+        }
+    }
+
+    #endregion
+
+    #region Photon Callback Methods
+    public override void OnConnected()
+    {
+        //    base.OnConnected();
+        Debug.Log("connected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        // base.OnConnectedToMaster();
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + "connected to photon");
+    }
+
+
+    #endregion
+}
